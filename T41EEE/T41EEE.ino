188743c1929f3f5f1a9fce5af6e7eac48399d073
Greg-R <3001539+Greg-R@users.noreply.github.com>
2024-03-02 12:27:20 -0500

    Updated .ino file to version T41EEE.4.

--- T41EEE/T41EEE.ino
+++ T41EEE/T41EEE.ino
@@ -1,5 +1,5 @@
 /*
-### Version T41EEE.3 T41 Software Defined Transceiver Arduino Sketch
+### Version T41EEE.4 T41 Software Defined Transceiver Arduino Sketch
 
 This is the "T41 Extreme Experimenter's Edition" software for the 
 T41 Software Defined Transceiver.  The T41EEE was "forked" from the V049.2 version
@@ -32,7 +32,7 @@ and feature enhancements become available.  You will be able to update and FLASH
 with the revised software quickly.
 
 Please note that the configuration structure is different than the predecessor V049.2
-It is recommended to perform a full FLASH erase before loading T41EEE.1.
+It is recommended to perform a full FLASH erase before loading T41EEE.4.
 
 You will need to install the ArduinoJSON library by Benoit Blanchon.  Using the IDE:
 Tools -> Manage Libraries ...
@@ -41,16 +41,45 @@ with the name Arduino_JSON, which is not the correct library.  Install the libra
 and if you have the other dependencies for the V049.2 version, compilation will be
 successful.
 
-## Highlight of Features included in T41EEE.3
+## Highlight of Changes included in T41EEE.4
 
-No new features are introduced in T41EEE.3.  This is a "clean-up" version.
-
-1.  Removal of extraneous #define statements.
-2.  Removal of unused variables.
-3.  Global variables which do not require global access are moved to their individual
-    files and are no longer extern (removed from SDT.h).
-4.  Some larger arrays are now DMAMEM.  This moves them to RAM2, thereby increasing the
-    stack size.
+1.   Moved CW and SSB power calculations out of loop().
+2.   Moved Morse decoder states to CWProcessing.cpp.
+3.   Fixed error in SSB and CW power set code.
+4.   User selected CW offset frequency shown in menu and saved to EEPROM.
+5.   Revised mic gain downward to 15 from 20.
+6.   TX equalizer amplitudes on and off close to the same.  TX and RX equalizer status to display.
+7.   CW filter selection was erasing graphics.
+8.   Fixed erasure of equalizer status when exiting calibration menus.
+9.   Re-inserted Len's switch matrix calibration by button press code.
+10.  Memory conservation by sharing buffers between RX and TX.
+11.  Adjusted Morse decoder to be less responsive to noise.
+12.  Re-normalization of CW and SSB power calibration.
+13.  Used arm functions to DC offset DAC outputs in the case of QSE2.
+14.  Added clipping function to receiver datastream.
+15.  Adjusted dataflow gain by 12 dB to increase volume.
+16.  Added AGC indicator.  
+17.  Increased CW decoding gain.  Earlier adjustment was too low.
+18.  Fixed AGC OFF indication.  RF gain set to 10dB when bandswitched.
+19.  Scaling factors for TX calibration were properly updated to match the CW exciter values.
+20.  Added a function to initialize the power arrays at boot.
+21.  Auto-gain upper limit set to 25.
+22.  Reduced Kim noise reduction gain.
+23.  Added a call to ResetHistograms() in setup.  This gets Morse decoder synced faster.
+24.  Updated SSB calibration defaults to higher. SSB exciter coefficient boosted.
+25.  Equalized TX gain paths (TX equalizer On versus Off).
+26.  Added new function initUserDefinedStuff().  Called after SD->EEPROM.
+27.  Adjusted CW and SSB encoder to maximum of 1.0.  This is completion of power calibration normalization.
+28.  Updated exciter code to use 2048 buffers.
+29.  Added DEBUG1 back in to allow temp and load display to work.
+30.  Moved the audio spectrum buffer to the stack due to users reporting audio problems.
+31.  Moved audioYPixel[] to the stack from DMAMEM in response to users reporting audio problems.
+32.  Added initializers (0) to audioSpectBuffer and audioYPixel.  This fixes audio spectrum glitch at power-up.
+33.  Restricted audioYPixel for loop due to array out-of-bounds.
+34.  Added CW zero scaling for periods between keying so that DC offset remains (affects QSE2 only);
+35.  Fixed problem with loud tone at power up with key down.
+36.  Added #ifdefs for QSE2.
+37.  Fixed problem with LMS noise conflict with AutoNotch.
 
 *********************************************************************************************
 
