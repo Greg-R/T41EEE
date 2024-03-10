0c354b9e7a9ea8d5bbef0b09813431d6784672d9
Greg-R <3001539+Greg-R@users.noreply.github.com>
2024-03-09 20:03:52 -0500

    Returned MyConfigurationFile.h to default.

--- T41EEE/MyConfigurationFile.h
+++ T41EEE/MyConfigurationFile.h
@@ -48,10 +48,10 @@
 //#define                           FOURSQRP
 
 // Set multiplication factors for your QSD and QSE boards.
-#define MASTER_CLK_MULT_RX 2
-#define MASTER_CLK_MULT_TX 2
+#define MASTER_CLK_MULT_RX 4
+#define MASTER_CLK_MULT_TX 4
 
 // Uncomment this line for QSE2.
-#define QSE2
+//#define QSE2
 
 
