830dc6d6289754769316f101d5f572ad0bf11892
Greg-R <3001539+Greg-R@users.noreply.github.com>
2024-10-30 13:29:33 -0400

    Needed to add #define FAST_TUNE after merge.

--- T41EEE/MyConfigurationFile.h
+++ T41EEE/MyConfigurationFile.h
@@ -6,6 +6,7 @@
 //#define DEBUG_SWITCH_CAL                                                  // Uncomment to run switch cal by pushing and holding a button at power-up.
                                                                             // Debug switch cal must be disabled for normal radio operation!
 //#define DEBUG_CESSB                                                       // Uncomment to get CESSB operating parameters printed to the serial monitor.
+//#define FAST_TUNE                                                         // Uncomment to activate variable speed fast tune by Harry GM3RVL.                             
 #define DEFAULT_KEYER_WPM   			15                                        // Startup value for keyer wpm
 #define FREQ_SEP_CHARACTER  			'.'					                              // Some may prefer period, space, or combo
 #define MAP_FILE_NAME   					"Cincinnati.bmp"                          // Name you gave to BMP map file. Max is 50 chars
