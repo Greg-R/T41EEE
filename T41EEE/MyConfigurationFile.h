96348fb7ddb017e32f2427742f845cd901045f8e
Greg-R <3001539+Greg-R@users.noreply.github.com>
2024-11-09 09:35:02 -0500

    Added DSPGAINSCALE parameter to MyConfigurationFile.h.

--- T41EEE/MyConfigurationFile.h
+++ T41EEE/MyConfigurationFile.h
@@ -76,6 +76,8 @@
 
 // If using an external amplifier, the gain may need to be adjusted for the best volume range.
 #define AUDIOSCALE  20    // A typical value is 20.  Increase or decrease this value depending on your amplifier gain.
-
+#define DSPGAINSCALE 10.0  // A typical value for a V10/V11 radio is in the range of 5 to 15.
+                           // Adjust this value for visible noise in the audio spectrum window with antenna connected
+                           // and RF Gain set to 0 dB.
 
 
