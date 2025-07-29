2f6e224cc5150af6ce5d00bb087359e2fc038d33
Greg-R <3001539+Greg-R@users.noreply.github.com>
2025-07-29 19:50:05 -0400

    Playing with receive auto-cal.

--- T41EEE/MyConfigurationFile.h
+++ T41EEE/MyConfigurationFile.h
@@ -86,4 +86,4 @@
 #define FREQUENCYCAL 130000  // The nominal frequency calibration.  This can be set here permanently after determining
                              // the unique value for your radio.
 
-#define RECEIVERAUTOCAL      //  Uncomment to activate automatic receiver calibration.
+//#define RECEIVERAUTOCAL      //  Uncomment to activate automatic receiver calibration.
