ffb74e0d817a4b4dc78ecc75f230b8f0b3b4c923
Greg-R <greg.electricity@gmail.com>
2025-11-20 17:12:18 -0500

    Increased default button delay.

--- T41EEE/SDT.h
+++ T41EEE/SDT.h
@@ -411,7 +411,7 @@ struct calibration_t {
 
   int buttonThresholdPressed = 944;   // switchValues[0] + WIGGLE_ROOM
   int buttonThresholdReleased = 964;  // buttonThresholdPressed + WIGGLE_ROOM
-  uint32_t buttonRepeatDelay = 300000;
+  uint32_t buttonRepeatDelay = 400000;
 #ifdef QSE2
   float32_t iDCoffsetCW[NUMBER_OF_BANDS] = { 0, 0, 0, 0, 0, 0, 0 };
   float32_t qDCoffsetCW[NUMBER_OF_BANDS] = { 0, 0, 0, 0, 0, 0, 0 };
